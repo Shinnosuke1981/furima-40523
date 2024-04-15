d651b147157c75775ddf59defe7aee49cd5df418
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-04-15 21:26:38 +0900

    新規登録成功２

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birth_date, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-ん一-龥々]+\z/, message: '漢字かひらがなを使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name_kanji
     validates :first_name_kanji
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_katakana
     validates :first_name_katakana
   end
