eeb2e2c67c3d2417bc14a3555aec611d05858460
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-04-17 15:07:52 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one :order
   has_one_attached :image
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :shipping_fee_responsibility
+  belongs_to :shipping_origin
+  belongs_to :estimated_shipping_date
+
+
+
+
+
+
 end
