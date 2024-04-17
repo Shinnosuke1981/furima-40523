39c99ace78f2e46bf94b6517cbf96ad75d09582d
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-04-17 21:32:42 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :item_price, :shipping_fee_responsibility_id, :comment, :item_category_id, :item_condition_id, :shipping_origin_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,10 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :title, presence: true
   validates :item_price, presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :shipping_fee_responsibility_id, numericality: { other_than: 1 } 
+  validates :shipping_fee_responsibility_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :comment, presence: true
-  validates :item_category_id, numericality: { other_than: 1 } 
-  validates :item_condition_id, numericality: { other_than: 1 } 
-  validates :shipping_origin_id, numericality: { other_than: 1 } 
-  validates :estimated_shipping_date_id, numericality: { other_than: 1 } 
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_origin_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :estimated_shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
