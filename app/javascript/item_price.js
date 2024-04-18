3c513d574bc5f041395322798c2f5a02bd5de545
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-04-18 21:00:07 +0900

    CR修正２

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,6 @@ window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
