1c3f4ac130cef87be84803fc54d877d6aeb66152
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-04-20 13:57:54 +0900

    商品編集画面に遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  private
+  def edit
+    @item = Item.find(params[:id])
+  end
 
+  private
   def item_params
     params.require(:item).permit(:title, :image, :item_price, :shipping_fee_responsibility_id, :comment, :item_category_id,
                                  :item_condition_id, :shipping_origin_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
