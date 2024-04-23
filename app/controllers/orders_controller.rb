80225246396542b41f3d8f865c135867452f39bf
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-04-23 20:33:35 +0900

    エラー文を表示済

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,19 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id]) 
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture, :city, :street_number, :building_name, :phone_number,:order_id).merge(user_id: current_user.id)
   end
 end
