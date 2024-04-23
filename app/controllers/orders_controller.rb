0b33d52c8f1bd102138e248c19f9547fee6a340d
Shinnosuke1981 <164172955+Shinnosuke1981@users.noreply.github.com>
2024-04-23 18:05:59 +0900

    商品購入画面編集と遷移済

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
 
-  def new
+  def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id]) 
   end
 
+  def create
+    binding.pry
+  end
 end
